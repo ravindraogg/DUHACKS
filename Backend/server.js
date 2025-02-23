fe31a7582f4e4266fc00b1abbd9d8e27d324f98c
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2025-02-23 21:19:48 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://cost-sage-du-hacks.netlify.app/", credentials: true }));
 app.use(express.json());
 
 mongoose
