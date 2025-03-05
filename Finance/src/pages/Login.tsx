d2080a2b468d4a7ffd6349e17a7691a5bbe14076
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2025-03-05 18:52:17 +0530

    Update Login.tsx

--- Finance/src/pages/Login.tsx
+++ Finance/src/pages/Login.tsx
@@ -25,7 +25,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post("https://cost-sage-backedn.netlify.app/api/login", formData);
+      const response = await axios.post("https://duhacks-p6t6.onrender.com/api/login", formData);
 
       if (response.data.success) {
         // Store token for authentication
